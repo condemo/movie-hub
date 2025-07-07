cbd26ffafdf2d402fb92e902701084f01d8ee068
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-07-07 20:23:50 +0200

    change: mockup data moved to tests

--- services/data_handler/service/service.go
+++ services/data_handler/service/service.go
@@ -7,47 +7,6 @@ import (
 	"github.com/condemo/movie-hub/services/common/store"
 )
 
-var mockupMovie pb.Media = pb.Media{
-	Id:          1,
-	Type:        "movie",
-	Title:       "Fake Movie",
-	Year:        1953,
-	Genres:      "acción,fantasía",
-	Seasons:     0,
-	Caps:        0,
-	Description: "Some desc",
-	Rating:      78,
-	Image:       "image.jpg",
-	Fav:         true,
-	Viewed:      false,
-}
-
-var mockupSerie pb.Media = pb.Media{
-	Id:          2,
-	Type:        "serie",
-	Title:       "Fake Serie",
-	Year:        1993,
-	Genres:      "acción,fantasía",
-	Seasons:     2,
-	Caps:        21,
-	Description: "Some desc",
-	Rating:      91,
-	Image:       "image.jpg",
-	Fav:         false,
-	Viewed:      false,
-}
-
-var mockupResume pb.MediaResume = pb.MediaResume{
-	Id:          1,
-	Type:        "movie",
-	Title:       "Fake Movie",
-	Genres:      "acción,fantasía",
-	Description: "Some desc",
-	Image:       "image.jpg",
-	Fav:         true,
-	Viewed:      false,
-}
-
 type DataService struct {
 	// Injections
 	store store.Store
@@ -57,23 +16,19 @@ func NewDataService(s store.Store) *DataService {
 	return &DataService{store: s}
 }
 
-func (s *DataService) GetLastMovies(ctx context.Context) *pb.MediaListResponse {
+func (s *DataService) GetLastUpdates(ctx context.Context) *pb.MediaListResponse {
 	// TODO: Esto debería devolver una lista de pelis
 	return &pb.MediaListResponse{
-		MediaList: []*pb.MediaResume{
-			&mockupResume,
-			&mockupResume,
-			&mockupResume,
-		},
+		MediaList: []*pb.MediaResume{},
 	}
 }
 
 func (s *DataService) GetMovie(ctx context.Context, id int64) *pb.Media {
 	// TODO:
-	return &mockupMovie
+	return nil
 }
 
 func (s *DataService) GetSerie(ctx context.Context, id int64) *pb.Media {
 	// TODO:
-	return &mockupSerie
+	return nil
 }
