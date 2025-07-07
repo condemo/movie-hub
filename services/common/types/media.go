0760271fabeee9167c683591f158dfbf77a3c8d0
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-07-07 20:21:38 +0200

    feat: added `GetProtoData()` to `Media` and `MediaResume`

--- services/common/types/media.go
+++ services/common/types/media.go
@@ -1,7 +1,9 @@
 package types
 
+import "github.com/condemo/movie-hub/services/common/protogen/pb"
+
 type Media struct {
-	ID          int64  `db:"id"`
+	Id          int64  `db:"id"`
 	Type        string `db:"type"`
 	Title       string `db:"title"`
 	Year        int32  `db:"year"`
@@ -15,8 +17,25 @@ type Media struct {
 	Viewed      bool   `db:"viewed"`
 }
 
+func (m Media) GetProtoData() *pb.Media {
+	return &pb.Media{
+		Id:          m.Id,
+		Type:        m.Type,
+		Title:       m.Type,
+		Year:        m.Year,
+		Genres:      m.Genres,
+		Seasons:     m.Seasons,
+		Caps:        m.Caps,
+		Description: m.Description,
+		Rating:      m.Rating,
+		Image:       m.Image,
+		Fav:         m.Fav,
+		Viewed:      m.Viewed,
+	}
+}
+
 type MediaResume struct {
-	ID          int64  `db:"id"`
+	Id          int64  `db:"id"`
 	Type        string `db:"type"`
 	Title       string `db:"title"`
 	Genres      string `db:"genres"`
@@ -24,4 +43,17 @@ type MediaResume struct {
 	Image       string `db:"image"`
 	Fav         bool   `db:"fav"`
 	Viewed      bool   `db:"viewed"`
+}
+
+func (m MediaResume) GetProtoData() *pb.MediaResume {
+	return &pb.MediaResume{
+		Id:          m.Id,
+		Type:        m.Type,
+		Title:       m.Title,
+		Genres:      m.Genres,
+		Description: m.Description,
+		Image:       m.Image,
+		Fav:         m.Fav,
+		Viewed:      m.Viewed,
+	}
 }
