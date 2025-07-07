a6a3ed3a5997c9eee813ed4c3f84175d6f9287a4
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-07-07 20:20:46 +0200

    feat: implemented `InsertBulkMedia` method to db

--- services/common/store/store.go
+++ services/common/store/store.go
@@ -1,9 +1,19 @@
 package store
 
-import "github.com/jmoiron/sqlx"
+import (
+	"context"
+
+	"github.com/condemo/movie-hub/services/common/types"
+	"github.com/jmoiron/sqlx"
+)
 
 // TODO:
-type Store interface{}
+type Store interface {
+	GetLastUpdates() ([]*types.MediaResume, error)
+	GetMovie(ctx context.Context, id int64) (*types.Media, error)
+	GetSerie(ctx context.Context, id int64) (*types.Media, error)
+	InsertBulkMedia([]types.Media) error
+}
 
 type Storage struct {
 	db *sqlx.DB
@@ -11,4 +21,42 @@ type Storage struct {
 
 func NewStorage(db *sqlx.DB) *Storage {
 	return &Storage{db: db}
+}
+
+func (s *Storage) GetLastUpdates() ([]*types.MediaResume, error) {
+	mr := []*types.MediaResume{}
+	err := s.db.Select(&mr, "SELECT * FROM media ORDER BY id DESC")
+	if err != nil {
+		return nil, err
+	}
+
+	return mr, nil
+}
+
+func (s *Storage) GetMovie(ctx context.Context, id int64) (*types.Media, error) {
+	movie := new(types.Media)
+	err := s.db.Get(movie, "SELECT * FROM media WHERE id=$1", id)
+	if err != nil {
+		return nil, err
+	}
+
+	return movie, nil
+}
+
+func (s *Storage) GetSerie(ctx context.Context, id int64) (*types.Media, error) {
+	// TODO:
+	return nil, nil
+}
+
+func (s *Storage) InsertBulkMedia(m []types.Media) error {
+	// TODO: recibir las rows de `NamedQuery`
+	_, err := s.db.NamedExec(`INSERT INTO media (type, title, year,
+		genres, seasons, caps, description, rating, image, fav, viewed)
+		VALUES (:type, :title, :year,:genres, :seasons,:caps,:description,:rating,
+		:image,:fav,:viewed)`, m)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
