b6eb23046cd16c9a866f36741c547f327e3283ea
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-07-21 20:14:56 +0200

    feat: added `RequestData` for persinst data needed to request movie api

---
+++ services/common/persistant/persistant.go
@@ -0,0 +1,58 @@
+package persistant
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/condemo/movie-hub/services/common/config"
+)
+
+var RequestData = newReqData()
+
+type reqData struct {
+	LastMediaDate *int64 `json:"lastMediaDate"`
+}
+
+func newReqData() reqData {
+	rd := reqData{}
+	if _, err := os.Stat(config.DefaultPaths.DataFile); os.IsNotExist(err) {
+		f, err := os.Create(config.DefaultPaths.DataFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		rd.LastMediaDate = nil
+		json.NewEncoder(f).Encode(&rd)
+
+	} else {
+		f, err := os.Open(config.DefaultPaths.DataFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		err = json.NewDecoder(f).Decode(&rd)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return rd
+}
+
+func (rd reqData) Save() error {
+	f, err := os.Create(config.DefaultPaths.DataFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	err = json.NewEncoder(f).Encode(rd)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
