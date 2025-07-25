44df914a56b39221123fb653439e7a922364a3df
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-07-25 22:37:50 +0200

    feat: new `FromPBMediaToTypeMedia` utility

--- services/common/utils/utils.go
+++ services/common/utils/utils.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"log"
 
+	"github.com/condemo/movie-hub/services/common/protogen/pb"
+	"github.com/condemo/movie-hub/services/common/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -14,4 +16,22 @@ func NewGrpcClient(addr string) *grpc.ClientConn {
 	}
 
 	return conn
+}
+
+func FromPBMediaToTypeMedia(pb *pb.Media) *types.Media {
+	return &types.Media{
+		Id:          pb.GetId(),
+		Type:        pb.GetType(),
+		Title:       pb.GetTitle(),
+		Year:        pb.GetYear(),
+		FirstAir:    pb.GetFirstAir(),
+		Genres:      pb.GetGenres(),
+		Seasons:     pb.GetSeasons(),
+		Caps:        pb.GetCaps(),
+		Description: pb.GetDescription(),
+		Rating:      pb.GetRating(),
+		Image:       pb.GetImage(),
+		Fav:         pb.GetFav(),
+		Viewed:      pb.GetViewed(),
+	}
 }
